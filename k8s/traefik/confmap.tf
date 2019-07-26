f908692f1aefc73bc19755511398a20edb1a4d08
RPalaziuk <vikingar111@gmail.com>
2019-07-26 17:30:02 +0300

    zatup5

--- k8s/traefik/confmap.tf
+++ k8s/traefik/confmap.tf
@@ -4,6 +4,6 @@ resource "kubernetes_config_map" "traefik_configmap" {
     namespace = "${kubernetes_namespace.traefik.metadata.0.name}"
   }
   data = {
-    "traefik.toml" = "${file("c:/users/rpalaz/downloads/forecastdemo/k8s/traefik/confmap.toml")}"
+    "traefik.toml" = "${file("./k8s/traefik/confmap.toml")}"
   }
 }
